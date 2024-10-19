8c4d0d87cf1aa79fd0752d5350941f93a2404896
Emmanuel Esan <nikolasdevs@gmail.com>
2024-10-19 21:33:39 +0100

    public folder created

---
+++ public/config.js
@@ -0,0 +1,10 @@
+const API_CLASS_ENDPOINT =
+  "http://cliniqueplushealthcare.com.ng/prescriptions/drug_class";
+
+const API_MEDICINE_ENDPOINT =
+  "https://cliniqueplushealthcare.com.ng/prescriptions/get_drug_class_by_id";
+
+const API_ALL_MEDICINE_ENDPOINT =
+  "https://cliniqueplushealthcare.com.ng/prescriptions/all_medicine/";
+
+export { API_CLASS_ENDPOINT, API_MEDICINE_ENDPOINT, API_ALL_MEDICINE_ENDPOINT };
