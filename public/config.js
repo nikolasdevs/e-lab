3b738779e8d2b918a4fcfee4901674cb3d119582
Emmanuel Esan <nikolasdevs@gmail.com>
2024-10-19 22:14:44 +0100

    config file updated

--- public/config.js
+++ public/config.js
@@ -1,12 +1,8 @@
 const config = {
-  API_CLASS_ENDPOINT:
-    process.env.API_CLASS_ENDPOINT ||
-    "http://default-api-url.com/prescriptions/drug_class",
+  API_CLASS_ENDPOINT: "http://default-api-url.com/prescriptions/drug_class",
   API_MEDICINE_ENDPOINT:
-    process.env.API_MEDICINE_ENDPOINT ||
     "http://default-api-url.com/prescriptions/get_drug_class_by_id",
   API_ALL_MEDICINE_ENDPOINT:
-    process.env.API_ALL_MEDICINE_ENDPOINT ||
     "https://cliniqueplushealthcare.com.ng/prescriptions/all_medicine/",
 };
 
